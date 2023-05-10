4950787f8032d4418875d180a0ea9286b99f927a
MirabdullaYuldashev <mirabdullayuldashev@gmail.com>
2023-05-10 16:31:21 +0600

    Home work 4

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CalculatorApp from './components/CalculatorApp';
+
+function App() {
+    return (
+        <div className="App">
+            <CalculatorApp/>
+        </div>
+    );
+}
+
+export default App;
